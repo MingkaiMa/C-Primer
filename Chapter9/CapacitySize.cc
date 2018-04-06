b674195be7bbf3305f248808f5ccd98294765a2b
Mingkai Ma <mamingkai824@gmail.com>
2018-04-06 16:32:56 +1000

    Chapter 9

---
+++ Chapter9/CapacitySize.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    vector<int> ivec;
+    cout << "ivec: size: " << ivec.size()
+        << " capacity: " << ivec.capacity() << endl;
+}
